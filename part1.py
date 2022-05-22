b77574eb61fe928c79ee8d77128c67ad101a895a
Jason Chan <103702120+jasonchan028@users.noreply.github.com>
2022-05-22 02:23:31 +0000

    did all parts

--- part1.py
+++ part1.py
@@ -14,14 +14,16 @@ Enter another number: 5
 
 The smallest number is 2
 '''
+smallest = 0
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+number3 = int(input("Enter another number: "))
 
-number = int(input("Enter a number: "))
-
-smallest = number
-
-number = int(input("Enter another number: "))
-
-if number < smallest:
-  smallest = number
+if number1 < number2 and number1 < number3:
+  smallest = number1
+elif number2 < number1 and number2 < number3:
+  smallest = number2
+else:
+  smallest = number3
   
 print("The smallest number is ", smallest)
--- part5.py
+++ part5.py
@@ -23,3 +23,14 @@ not a month
 '''
 
 #start writing your code below
+
+month = str(input("Enter a month: "))
+monthlist = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+if month not in monthlist:
+  print("not a month")
+else:
+ if monthlist.index(month) % 2 == 0 or monthlist.index(month) == 7:
+  print("31")
+ else:
+  print("28 or 29")
