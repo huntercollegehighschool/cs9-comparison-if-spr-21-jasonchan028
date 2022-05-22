e2fd70297b0b709017c26cb2ccf22744e6dae723
Jason Chan <103702120+jasonchan028@users.noreply.github.com>
2022-05-22 15:38:38 +0000

    added yet another elif statement to pt 1

--- part1.py
+++ part1.py
@@ -23,6 +23,8 @@ if number1 < number2 and number1 < number3:
   smallest = number1
 elif number2 < number1 and number2 < number3:
   smallest = number2
+elif number3 < number1 and number3 < number2:
+  smallest = number3
 elif number1 == number2 or number1 == number3:
   smallest = number1
 else:
